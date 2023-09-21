fa188d8d7c2ef2c646426fac7eb50c64981eebaa
andreaconv <andrea2convertini@gmail.com>
2023-09-21 15:06:44 +0200

    [add] BONUS esercizio 1

--- src/org/java/es1/Main.java
+++ src/org/java/es1/Main.java
@@ -1,8 +1,10 @@
 package org.java.es1;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -13,23 +15,32 @@ public class Main {
 		
 		boolean finish = true;
 		
-		List<String> regali = new ArrayList<>();
+		//cambio struttura da List a Map che rappresenta una collezione di coppie "chiave-valore"
+		Map<String, String> regali = new HashMap<>();
 		
-		System.err.println("Inserisci il nome del regalo altrimenti scrivi 'STOP' se vuoi concludere \n");
+		System.err.println("Inserisci il nome del regalo e successivamente il destinatario altrimenti scrivi 'STOP' se vuoi concludere \n");
 		do {
 			System.out.print("Nome del regalo: ");
-			String input = sc.nextLine();
+			String regalo = sc.nextLine();
 			
 			//condizione per concludere il ciclo
-			if(input.toLowerCase().equals("stop")) {
+			if(regalo.toLowerCase().equals("stop")) {
 				finish = false;
-				System.out.println("La lista che hai scritto è questa: ");
 				
-				System.out.println(regali);
+				System.out.println("\nLa lista che hai scritto è questa: \n");
+				
+				//stampo gli elementi
+				for (String key : regali.keySet()) {
+				    System.out.println(key + ": " + regali.get(key) + "\n");
+				}
 				
 			} else {
 				
-				regali.add(input);
+				System.out.print("Nome del destinatario: ");
+				String destinatario = sc.nextLine();
+				
+				//array -- chiave ----- valore
+				regali.put(destinatario, regalo);
 				
 				System.out.println("La lista regali contiene: " + regali.size() + ((regali.size() <= 1) ? " regalo" : " regali"));
 				System.out.println("\n----------------------------------\n");
